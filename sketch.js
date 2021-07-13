67a9d24884b7fff2ec6559a76c3a0160e49f20f0
khushijain666 <84087610+khushijain666@users.noreply.github.com>
2021-07-13 19:21:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,41 @@
-var form,player,game
-var gameState=0
-var playerCount
-var database;
+//Create variables here
 
-function setup(){
-    createCanvas(4000,400);
-    database=firebase.database();
-    game=new Game()
-    game.getState()
-    game.start()
-    
+function preload()
+{
+	//load images here
+dog.loadImage
+ 
 }
 
-function draw(){
-    
+function setup() {
+	createCanvas(500,500);
+  
 }
+
+
+function draw() {
+  background()
+    color.colorcode(46,139,87)
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogHappy)
+  }
+  
+
+drawSprites();
+fill(red)
+stroke()
+  //add styles here
+  dog=12
+  happyDog=5
+  database=2
+  foodS=9
+  foodStock=6
+  foodStockdatabase.ref("Food")
+  foodStock.on("value,readStock")
+
+}
+
+
+
