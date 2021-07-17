3fb4974b2f289927b0ec90ecd3197b2b9036b86d
khushijain666 <84087610+khushijain666@users.noreply.github.com>
2021-07-17 20:49:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,37 +3,18 @@
 function preload()
 {
 	//load images here
-dog.loadImage
- 
 }
 
 function setup() {
-	createCanvas(500,500);
+	createCanvas(800, 700);
   
 }
 
 
-function draw() {
-  background()
-    color.colorcode(46,139,87)
+function draw() {  
 
-  if(keyWentDown(UP_ARROW)){
-    writeStock(foodS);
-    dog.addImage(dogHappy)
-  }
-  
-
-drawSprites();
-fill(red)
-stroke()
+  drawSprites();
   //add styles here
-  dog=12
-  happyDog=5
-  database=2
-  foodS=9
-  foodStock=6
-  foodStockdatabase.ref("Food")
-  foodStock.on("value,readStock")
 
 }
 
